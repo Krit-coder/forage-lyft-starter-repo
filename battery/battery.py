875b3ec5072014993d1efc5c78b99e280fc4e6e1
Krit Raj <kritraj.01@gmail.com>
2023-07-12 10:02:25 +0530

    Commit2

--- battery/battery.py
+++ battery/battery.py
@@ -4,3 +4,4 @@ from abc import ABC
 class Battery(ABC):
     def needs_service(self):
         pass
+    
---
+++ car_factory.py
@@ -0,0 +1,43 @@
+from battery.nubbin_battery import NubbinBattery
+from battery.splinder_battery import SplinderBattery
+from engine.capulet_engine import CapuletEngine
+from engine.willoughby_engine import WilloughbyEngine
+from engine.sternman_engine import SternmanEngine
+
+
+class CarFactory:
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage,last_service_mileage)
+        battery = SplinderBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    @staticmethod
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = SplinderBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    @staticmethod
+    def create_palindrome(current_date, last_service_date, warning_lights):
+        engine = SternmanEngine(warning_lights)
+        battery = SplinderBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    @staticmethod
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    @staticmethod
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
