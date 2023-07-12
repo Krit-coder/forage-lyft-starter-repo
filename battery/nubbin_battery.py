4cda9d8fb3996fde1deada7b5c72ed69e4da2e0f
Krit Raj <kritraj.01@gmail.com>
2023-07-12 09:26:28 +0530

    Commit1

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,15 @@
+from battery import Battery
+from util import add_years_to_date
+
+
+class NubbinBattery(Battery):
+    def __int__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        date_when_to_service = add_years_to_date(self.last_service_date, 4)
+        if date_when_to_service < self.current_date:
+            return True
+        else:
+            return False
--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,15 +1,12 @@
-from abc import ABC
-
-from car import Car
+from engine import Engine
 
 
-class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
-        self.warning_light_is_on = warning_light_is_on
+class SternmanEngine(Engine):
+    def __init__(self, warning_lights):
+        self.warning_lights = warning_lights
 
-    def engine_should_be_serviced(self):
-        if self.warning_light_is_on:
+    def needs_service(self):
+        if self.warning_lights:
             return True
         else:
             return False
